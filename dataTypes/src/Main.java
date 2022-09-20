b1c638966c271fa474b2c2d59a9cd1cf1e4c72f1
Ruken Erpolat <72344293+rukenerpolat@users.noreply.github.com>
2022-09-20 14:44:04 +0300

    datatypes

---
+++ dataTypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double sayi = 12.5;
+        double sayi2 = 12.6;
+        System.out.println(sayi + sayi2);
+        //Yazılımcılar genellikle int ve double kullanır.
+        char karakter = 'A'; //Sadece tek bir karakter tutar.
+        boolean dogruMu=false;
+
+    }
+}

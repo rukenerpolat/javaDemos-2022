e444e537b06560029c9afa373ab2997150a5e3e7
Ruken Erpolat <72344293+rukenerpolat@users.noreply.github.com>
2022-09-22 14:47:21 +0300

    classes

---
+++ classes/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        //referance type
+        CustomerManager customerManager = new CustomerManager();  //Garbage Collector
+        CustomerManager customerManager2 = new CustomerManager();
+        customerManager = customerManager2;
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+
+        /*
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+        */
+
+        // value type
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1 = 30;
+        System.out.println(sayi2);  // sayi2 = 10 olur. value type
+
+        int[] sayilar1 = new int[]{1,2,3};
+        int[] sayilar2 = new int[]{4,5,6};
+        sayilar2 = sayilar1;
+        sayilar1[0]=10;
+        System.out.println(sayilar2[0]);
+        //new'ledik ve heap kısmında yer açtık.
+        //referans tuttuğu için, referans tip gibi düşünülür ve sonuç 10 olur yine.
+
+    }
+}
+

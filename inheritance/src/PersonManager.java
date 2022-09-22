4a57f063ee3a4f738c957464069c184637b4a76a
Ruken Erpolat <72344293+rukenerpolat@users.noreply.github.com>
2022-09-22 17:51:23 +0300

    inheritance

---
+++ inheritance/src/PersonManager.java
@@ -0,0 +1,9 @@
+public class PersonManager {
+    public void List(){
+        System.out.println("Müsteri listelendi!");
+    }
+
+    public void Add(){
+        System.out.println("Müsteri eklendi!");
+    }
+}
